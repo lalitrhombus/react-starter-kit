78bd38070cc900ff40c2c3242f8bbf15e875abbb
Lalit Chaturvedi <lalitc@babajob.com>
2016-05-13 02:39:05 +0530

    removing the content folder and config value changes

--- src/config.js
+++ src/config.js
@@ -17,7 +17,7 @@ export const analytics = {
 
 export const auth = {
 
-  jwt: { secret: process.env.JWT_SECRET || 'React Starter Kit' },
+  jwt: { secret: process.env.JWT_SECRET || 'Scorecard' },
 
   // https://developers.facebook.com/
   facebook: {
